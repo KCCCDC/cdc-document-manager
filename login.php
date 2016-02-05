4d6ca2f676a75dbf05929362833ca57ecf8e58da
Jacob Moody <sinful.spartan@gmail.com>
2016-02-05 00:28:37 -0600

    Forgot to update these before inital commit, follow first commit in branch to see purpose

--- login.php
+++ login.php
@@ -1,6 +1,15 @@
 <html>
     <?php include 'openDB.php' ?>
     <?php
-       
+        if(isset($_POST['user']) && isset($_POST['passwd']) ){
+            $username = $_POST['user'];
+            $query = $db->prepare('select * from users where username=:username');
+            $query->execute(array(':username' => $username));
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+            $login = password_verify($_POST['passwd'], $result['pass']);
+            echo($login);
+        }
     ?>    
+
+<? include 'closeDB.php'?>
 </html>
