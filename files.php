e2127d817a8b26a1ffaacd27bb86a110011c7f70
Jacob Moody <sinful.spartan@gmail.com>
2016-02-05 22:27:56 -0600

    Added basic file crawling, lists files out of main dir.  Allows use of sub dirs as well.  Also updated .gitignore to ignore log files

--- files.php
+++ files.php
@@ -1,6 +1,34 @@
 <?php
+    function  printFiles($parentPath){
+        $directory = array_diff(scandir($parentPath), array('..', '.'));
+        $leftNode[] = NULL;
+        echo("<form action='download.php' method='post'> ");
+        foreach($directory as $child){
+            if(!is_dir($parentPath . $child)){
+                echo($child . "<input type='radio' name='file' value=' " . $child . " ' >" . "<br>");
+            }else{
+                array_push($leftNode, $child);
+            }
+        }
+        echo("<input type='submit' value='Download''>");
+        echo("</form>");
+        foreach($leftNode as $dir){
+            if($dir!=NULL){
+                echo("-----" . $dir . "-----<br>");
+                printFiles($parentPath . $dir . '/');
+            
+            }
+        }
+    }
+    
     session_start();
+    $fileStorage = '/srv/files/';
     if(isset($_SESSION['user'])){
         echo('Welcome ' . $_SESSION['user'] . "<br>");
-        echo("You are Logged In!");
+        echo("You are Logged In!" . "<br>");
+        echo("<br> Available Files: <br>");
+        printFiles($fileStorage);
+    }else{
+        header('Location: /index.php');
     }
+?>
