3886d4990e0e78c091fc5b72aa376d11b39e1f26
Jacob Moody <sinful.spartan@gmail.com>
2016-02-08 22:49:33 -0600

    Forgot to add the upload script in last push

---
+++ upload.php
@@ -0,0 +1,42 @@
+<?php
+    function getDir($key){
+        include 'openDB.php';
+        $query = $db->prepare('select dir from dirs where key=:key');
+        $query->bindValue(':key', $key, PDO::PARAM_STR);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        return $result['dir'];
+        include 'closeDB.php';
+    }
+
+    function insertFile($filePath){
+        include 'openDB.php';
+        $key = hash('sha256', $filePath );
+        $user = $_SESSION['user'];
+        $insert = $db->prepare("insert into files (file, key, users) values ('$filePath', '$key', '$user')" );
+        $insert->execute();
+        include 'closeDB.php';
+    }
+
+    session_start();
+    if(isset($_SESSION['user']) && isset($_POST['dir']) ){
+        include 'config.php';
+        $uploadDir = getDir($_POST['dir']);
+
+        $errors = array();
+        $file_name = $_FILES['uploadFile']['name'];
+        $file_size =$_FILES['uploadFile']['size'];
+        $file_tmp =$_FILES['uploadFile']['tmp_name'];
+        $file_type=$_FILES['uploadFile']['type'];
+    
+        if($file_size > $maxUploadSize) $errors[] ="File is too large";
+
+        if(empty($errors)==true){
+            move_uploaded_file($file_tmp, $uploadDir . $file_name);
+            insertFile($uploadDir . $file_name);
+            header("Location: /files.php");
+        
+        }
+    }
+
+?>
